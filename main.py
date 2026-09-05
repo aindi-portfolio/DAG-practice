b05fa5c48cef8a359819b8738ed6df72e5e7ab7a
Aindi Tsarni <aindi.tsarni@gmail.com>
2026-09-05 11:41:01 -0400

    added 'app' to services in compose.yaml so that the dotenv files are loaded to get the API URL. Added comments to main.py.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
+# Load environment variables from .env file
 api_url = os.getenv("API_URL")
 database_host = os.getenv("DATABASE_HOST")
 database_port = os.getenv("DATABASE_PORT")
@@ -11,16 +12,19 @@ database_name = os.getenv("DATABASE_NAME")
 database_user = os.getenv("DATABASE_USER")
 database_password = os.getenv("DATABASE_PASSWORD")
 
+# Make a GET request to the API with a timeout of 30 seconds
 response = requests.get(api_url, timeout=30)
 response.raise_for_status()
 
+# Convert the response to JSON format
 response = response.json()
 
 print(f"Database Name: {database_name}\nDatabase Host: {database_host}\nDatabase Port: {database_port}\nDatabase User: {database_user}")
 print("/-----------------------------------/")
 print(f"API Response: {response}")
 
-
+# Insert the API response into the PostgreSQL database
+# Use "with" statement to ensure the connection is closed after use
 with psycopg2.connect(
     host=database_host,
     port=database_port,
@@ -28,6 +32,7 @@ with psycopg2.connect(
     user=database_user,
     password=database_password
 ) as db_conn:
+    # Use "cursor" to execute SQL commands within the context of the database connection"
     with db_conn.cursor() as cur:
         # Execute your queries here
         cur.execute(
