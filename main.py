afb5375d306b9bee0b84e340acf0407a83c20e60
Aindi Tsarni <aindi.tsarni@gmail.com>
2026-09-05 11:03:39 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import psycopg2
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+api_url = os.getenv("API_URL")
+database_host = os.getenv("DATABASE_HOST")
+database_port = os.getenv("DATABASE_PORT")
+database_name = os.getenv("DATABASE_NAME")
+database_user = os.getenv("DATABASE_USER")
+database_password = os.getenv("DATABASE_PASSWORD")
+
+response = requests.get(api_url, timeout=30)
+response.raise_for_status()
+
+response = response.json()
+
+print(f"Database Name: {database_name}\nDatabase Host: {database_host}\nDatabase Port: {database_port}\nDatabase User: {database_user}")
+print("/-----------------------------------/")
+print(f"API Response: {response}")
+
+
+with psycopg2.connect(
+    host=database_host,
+    port=database_port,
+    dbname=database_name,
+    user=database_user,
+    password=database_password
+) as db_conn:
+    with db_conn.cursor() as cur:
+        # Execute your queries here
+        cur.execute(
+            """
+            INSERT INTO raw_api_data (source, payload)
+            VALUES (%s, %s)
+            """,
+            (api_url, response)
+        )
+        print("API data inserted into PostgreSQL database successfully.")
